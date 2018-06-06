bcb17d5f9b408c680ba0b8db6d2f66f649b445bb
baardl <bard.lind@gmail.com>
2018-06-06 10:28:22 +0200

    Self-executing-jar

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # axon-flow-graph
 Visualize the flow of commands and events in an Axon Framework service
+
+## Generate dependency json
+
+```
+mvn clean install
+java -jar target/axon-flow-graph-.jar
+```
+
+Output will be in the file axon-flow-graph.json
---
+++ src/main/java/io/baardl/axon/graph/Main.java
@@ -0,0 +1,17 @@
+package io.baardl.axon.graph;
+
+import org.slf4j.Logger;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+/**
+ * Analyze the flow of commands and events.
+ * Default is in io.baardl.axon.action package.
+ */
+public class Main {
+    private static final Logger log = getLogger(Main.class);
+    public static void main(String[] args) {
+        log.info("Start");
+        log.info("Done");
+    }
+}
