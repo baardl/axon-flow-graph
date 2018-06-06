a5598dbf879b11d44b95248a4868163d630df544
baardl <bard.lind@gmail.com>
2018-06-06 11:00:21 +0200

    Find classes with CommandHandler and EventHandler annotations.

--- src/main/java/io/baardl/axon/graph/Main.java
+++ src/main/java/io/baardl/axon/graph/Main.java
@@ -1,6 +1,11 @@
 package io.baardl.axon.graph;
 
+import io.github.lukehutch.fastclasspathscanner.FastClasspathScanner;
+import org.axonframework.commandhandling.CommandHandler;
+import org.axonframework.eventhandling.EventHandler;
 import org.slf4j.Logger;
+
+import java.util.List;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
@@ -10,8 +15,26 @@ import static org.slf4j.LoggerFactory.getLogger;
  */
 public class Main {
     private static final Logger log = getLogger(Main.class);
+
+    void scan(String packageName) {
+        scanByAnnotation(packageName, CommandHandler.class);
+        scanByAnnotation(packageName, EventHandler.class);
+
+    }
+
+    private void scanByAnnotation(String packageName, Class annotationClass) {
+        final List<String> annotatedClasses = new FastClasspathScanner(packageName)
+                .enableMethodAnnotationIndexing().scan()
+                .getNamesOfClassesWithMethodAnnotation(annotationClass.getName());
+        for (String className : annotatedClasses) {
+            log.info("{} has annotation {}", className, annotationClass.getName());
+        }
+    }
+
     public static void main(String[] args) {
         log.info("Start");
+        Main main = new Main();
+        main.scan("io.baardl.axon.action");
         log.info("Done");
     }
 }
