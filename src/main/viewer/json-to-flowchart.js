d8bb7e4ec412c203c532ffff917aaf9bffb90091
Bard Lind <bard.lind@nrk.no>
2018-06-08 09:17:53 +0200

    Fetch json from file

--- src/main/viewer/json-to-flowchart.js
+++ src/main/viewer/json-to-flowchart.js
@@ -1,7 +1,15 @@
-
-
+async function fetchJson() {
+  var jsonobj = null;
+  $.getJSON("axon-flow-graph-template.json", function (data) {
+    jsonobj = data;
+  });
+  return jsonobj;
+}
 
 function findStart() {
+  var mydata = fetchJson();
+console.log("mydata: ", mydata);
+
   return 'st=>start: sstart:>http://www.google.com[blank]\n';
 }
 
