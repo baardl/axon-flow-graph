76ec238f1cf5e8152b21df48db69838d1f8cdd84
Bard Lind <bard.lind@nrk.no>
2018-06-08 09:32:21 +0200

    Async fetch

--- src/main/viewer/json-to-flowchart.js
+++ src/main/viewer/json-to-flowchart.js
@@ -1,16 +1,22 @@
-async function fetchJson() {
-  var jsonobj = null;
-  $.getJSON("axon-flow-graph-template.json", function (data) {
-    jsonobj = data;
-  });
-  return jsonobj;
+function fetchJson() {
+  return fetch("axon-flow-graph-template.json")
+    .then(data=>data.json())
 }
 
-function findStart() {
-  var mydata = fetchJson();
-console.log("mydata: ", mydata);
+async function findStart() {
+  var mydata = await fetchJson();
+  console.log("mydata: ", mydata);
+  /*
+  {
+      "name": "io.baardl.axon.action.ActionService",
+      "type": "CommandGateway",
+      "method_name": "create",
+      "next": "io.baardl.axon.action.CreateActionCommand"
+    }
+   */
 
-  return 'st=>start: sstart:>http://www.google.com[blank]\n';
+  var name = mydata.start[0].name;
+  return 'st=>start: ' + name + ':>http://www.google.com[blank]\n';
 }
 
 function findEnd() {
