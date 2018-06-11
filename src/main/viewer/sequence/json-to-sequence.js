2064d508e571a3d5667c5a1abe3cc5cecb9d2371
Bard Lind <bard.lind@nrk.no>
2018-06-11 13:16:45 +0200

    Draw start and nodes

--- src/main/viewer/sequence/json-to-sequence.js
+++ src/main/viewer/sequence/json-to-sequence.js
@@ -6,7 +6,7 @@ function fetchJson() {
 async function buildFlow() {
   var mydata = await fetchJson();
   // var flow_described = findStart(mydata) + findEnd(mydata) + findNodes(mydata) + findFlow(mydata);
-  var flow_described = findStart(mydata);
+  var flow_described = findStart(mydata) + findNodes(mydata);
   console.log("Flow: ", flow_described);
   return flow_described;
 
@@ -38,26 +38,15 @@ function findNodes(mydata) {
   for(var i in mydata.command_handlers) {
     var id = mydata.command_handlers[i].name;
     var name = mydata.command_handlers[i].name;
-    nodes = nodes + id + '=>operation: ' + name + '\n';
+    nodes = nodes + 'participant ' + id + '\n';
   }
 
   for(var i in mydata.event_handlers) {
     var id = mydata.event_handlers[i].name;
     var name = mydata.event_handlers[i].name;
-    nodes = nodes + id + '=>subroutine: ' + name + '\n';
+    nodes = nodes + 'participant ' + id + '\n';
   }
-
-   /*
-  var nodes = 'op1=>operation: My Operation\n' +
-  'op2=>operation: Stuff|current\n' +
-  'sub1=>subroutine: My Subroutine\n' +
-  'cond=>condition: Yes \n' + // use cond(align-next=no) to disable vertical align of symbols below
-  'or No?\n:>http://www.google.com\n' +
-  'c2=>condition: Good idea|rejected\n' +
-  'io=>inputoutput: catch something...|request\n';
-  */
   return nodes;
-
 }
 
 function findFlow(mydata) {
@@ -65,9 +54,8 @@ function findFlow(mydata) {
   var flow = "";
   for (var startIndex in mydata.start) {
     var start_id = mydata.start[startIndex].name;
-    // flow = flow + start_id + '->io.baardl.axon.action.ActionCommandHandler->io.baardl.axon.action.ActionEventObserver->e \n '
     var handler = findCommandHandler(mydata,mydata.start[startIndex].next);
-    flow = flow + start_id + '->' +handler + '\n';
+    flow = flow + start_id + '-->' +handler + '\n';
   }
 
   for (const commandHandler of mydata.command_handlers) {
